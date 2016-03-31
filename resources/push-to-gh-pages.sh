dbf85d661c8d4009182bbbd3a5744268b5e196ae
Weiming Zhai <wmzhai@gmail.com>
2016-03-31 08:48:22 +0800

    未来工作

--- resources/push-to-gh-pages.sh
+++ resources/push-to-gh-pages.sh
@@ -25,4 +25,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "git@github.com:wmzhai/mantra-zh.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "git@github.com:meteor-works/mantra-zh.git" master:gh-pages > /dev/null 2>&1
